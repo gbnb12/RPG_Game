d93878b7f64fd9ea22ad5c63b3bc74d1eb61b517
N <adamsnoah999@gmail.com>
2022-12-03 19:20:00 -0600

    Worked on UI and Heal objects/animation

--- Assets/RPG_Game/Scripts/StateMachine/ThreeStages/AITurn.cs
+++ Assets/RPG_Game/Scripts/StateMachine/ThreeStages/AITurn.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AITurn : TurnGameState
 {
@@ -13,6 +14,10 @@ public class AITurn : TurnGameState
     [SerializeField] Text _AIHealTextUI = null;
     [SerializeField] Text _AILaserTextUI = null;
 
+    [SerializeField] Image _AttackImage = null;
+    [SerializeField] Image _HealImage = null;
+    [SerializeField] Image _LaserImage = null;
+
     [SerializeField] float _pauseDuration = 1.5f;
 
     [SerializeField] Text _enemyLaserTextUI = null;
@@ -20,9 +25,21 @@ public class AITurn : TurnGameState
     [SerializeField] Collider _playerPiece;
     [SerializeField] Collider _AIPiece;
 
+    [SerializeField] Text _AttackDamageTextUI = null;
+    [SerializeField] Text _HealDamageTextUI = null;
+    [SerializeField] Text _LaserDamageTextUI = null;
+
+    [SerializeField] Image _AttackDamageImage = null;
+    [SerializeField] Image _HealDamageImage = null;
+    [SerializeField] Image _LaserDamageImage = null;
+
     private int _laserCountdown = 0;
 
+    public CameraShaking cameraShake;
+
     int _enemyAction = 0;
+
+    public GameObject healObject;
 
     public override void Enter()
     {
@@ -43,6 +60,18 @@ public class AITurn : TurnGameState
         _AIAttackTextUI.gameObject.SetActive(false);
         _AIHealTextUI.gameObject.SetActive(false);
         _AILaserTextUI.gameObject.SetActive(false);
+        _AttackImage.gameObject.SetActive(false);
+        _HealImage.gameObject.SetActive(false);
+        _LaserImage.gameObject.SetActive(false);
+
+        _AttackDamageTextUI.gameObject.SetActive(false);
+        _HealDamageTextUI.gameObject.SetActive(false);
+        _LaserDamageTextUI.gameObject.SetActive(false);
+        _AttackDamageImage.gameObject.SetActive(false);
+        _HealDamageImage.gameObject.SetActive(false);
+        _LaserDamageImage.gameObject.SetActive(false);
+
+        healObject.SetActive(false);
     }
 
     IEnumerator EnemyThinkingRoutine(float pauseDuration)
@@ -57,16 +86,31 @@ public class AITurn : TurnGameState
         {
             AIPressedAttack();
             _AIAttackTextUI.gameObject.SetActive(true);
+            _AttackImage.gameObject.SetActive(true);
+
+            _AttackDamageTextUI.gameObject.SetActive(true);
+            _AttackDamageImage.gameObject.SetActive(true);
         }
         if (_enemyAction == 2)
         {
             AIPressedHeal();
             _AIHealTextUI.gameObject.SetActive(true);
+            _HealImage.gameObject.SetActive(true);
+
+            _HealDamageTextUI.gameObject.SetActive(true);
+            _HealDamageImage.gameObject.SetActive(true);
+
+            healObject.SetActive(true);
         }
         if (_enemyAction == 3)
         {
             AIPressedLaser();
             _AILaserTextUI.gameObject.SetActive(true);
+            _LaserImage.gameObject.SetActive(true);
+
+            _LaserDamageTextUI.gameObject.SetActive(true);
+            _LaserDamageImage.gameObject.SetActive(true);
+
             _enemyAction -= 3;
         }
 
@@ -81,6 +125,7 @@ public class AITurn : TurnGameState
         if (damage != null)
         {
             damage.TakeAttackDamage(20);
+            //StartCoroutine(cameraShake.Shake(0.015f, -0.010f));
         }
         StateMachine.ChangeState<PlayerTurn>();
     }
@@ -92,6 +137,7 @@ public class AITurn : TurnGameState
         if (number != null)
         {
             number.TakeLaserDamage(30);
+            //StartCoroutine(cameraShake.Shake(0.015f, -0.010f));
             _laserCountdown -= 3;
             _enemyLaserTextUI.GetComponent<Text>().text = "Laser: " + _laserCountdown;
         }
--- Assets/RPG_Game/Scripts/StateMachine/ThreeStages/PlayerTurn.cs
+++ Assets/RPG_Game/Scripts/StateMachine/ThreeStages/PlayerTurn.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class PlayerTurn : TurnGameState
 {
     [SerializeField] Text _playerTurnTextUI = null;
     [SerializeField] Text _laserTextUI = null;
+
+    [SerializeField] Text _AttackDamageTextUI = null;
+    [SerializeField] Text _HealDamageTextUI = null;
+    [SerializeField] Text _LaserDamageTextUI = null;
+
+    [SerializeField] Image _AttackDamageImage = null;
+    [SerializeField] Image _HealDamageImage = null;
+    [SerializeField] Image _LaserDamageImage = null;
 
     int _playerTurnCount = 0;
 
@@ -15,6 +24,9 @@ public class PlayerTurn : TurnGameState
     [SerializeField] Collider _AIPiece;
 
     private int _laserCountdown = 0;
+
+    public GameObject healObject01;
+    public GameObject healObject02;
 
 
     public override void Enter()
@@ -51,6 +63,16 @@ public class PlayerTurn : TurnGameState
         StateMachine.Input.PressedLaser -= OnPressedLaser;
         StateMachine.Input.PressedHeal -= OnPressedHeal;
 
+        _AttackDamageTextUI.gameObject.SetActive(false);
+        _HealDamageTextUI.gameObject.SetActive(false);
+        _LaserDamageTextUI.gameObject.SetActive(false);
+        _AttackDamageImage.gameObject.SetActive(false);
+        _HealDamageImage.gameObject.SetActive(false);
+        _LaserDamageImage.gameObject.SetActive(false);
+
+        healObject01.SetActive(false);
+        healObject02.SetActive(false);
+
         Debug.Log("Player Turn; Exit");
     }
 
@@ -64,10 +86,13 @@ public class PlayerTurn : TurnGameState
     void OnPressedAttack()
     {
         Debug.Log("Player attacks the AI!");
+        
         IDamageable damage = _AIPiece.GetComponent<IDamageable>();
         if (damage != null)
         {
             damage.TakeAttackDamage(20);
+            _AttackDamageTextUI.gameObject.SetActive(true);
+            _AttackDamageImage.gameObject.SetActive(true);
         }
         StateMachine.ChangeState<AITurn>();
     }
@@ -75,11 +100,14 @@ public class PlayerTurn : TurnGameState
     void OnPressedLaser()
     {
         Debug.Log("Player shoots laser!");
+        
         IDamageable number = _AIPiece.GetComponent<IDamageable>();
         if (number != null)
         {
             number.TakeLaserDamage(30);
             _laserCountdown -= 3;
+            _LaserDamageTextUI.gameObject.SetActive(true);
+            _LaserDamageImage.gameObject.SetActive(true);
             _laserTextUI.GetComponent<Text>().text = "Laser: " + _laserCountdown;
         }
         StateMachine.ChangeState<AITurn>();
@@ -88,9 +116,13 @@ public class PlayerTurn : TurnGameState
     void OnPressedHeal()
     {
         Debug.Log("Player recovers!");
+        healObject01.SetActive(true);
+        healObject02.SetActive(true);
         IDamageable recover = _playerPiece.GetComponent<IDamageable>();
         if (recover != null)
         {
+            _HealDamageTextUI.gameObject.SetActive(true);
+            _HealDamageImage.gameObject.SetActive(true);
             recover.Heal(10);
         }
         StateMachine.ChangeState<AITurn>();
